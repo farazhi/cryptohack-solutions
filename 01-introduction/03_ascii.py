4367e747b8e3b1b7705c364fe7ec65345747e641
Jorge Farage <faragejorge74@gmail.com>
2026-08-09 19:31:35 -0300

    update

--- 01-introduction/03_ascii.py
+++ 01-introduction/03_ascii.py
@@ -5,7 +5,6 @@
 lista = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
 
 print("Flag: ", end="")
-
 # Percorre cada inteiro da lista e o transforma em caractere da tabela ASCII
 for caractere in lista:
     print(chr(caractere), end="")
