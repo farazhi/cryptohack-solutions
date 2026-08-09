98b723cc89792acb315a7335cf786b872f95b3f0
Jorge Farage <faragejorge74@gmail.com>
2026-08-09 19:07:49 -0300

    update

--- 01-introduction/01_finding_flags.py
+++ 01-introduction/01_finding_flags.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
+""" CryptoHack - Introduction - Finding Flags """
 
-"""
-CryptoHack - Introduction
-Challenge: Finding Flags
-"""
-
-def solve():
-    # Desafio introdutório: a flag é fornecida diretamente no enunciado.
-    flag = "crypto{y0ur_f1rst_fl4g}"
-    return flag
-
-if __name__ == "__main__":
-    print(f"🚩Flag: {solve()}")
+# Como o desafio é introdutório, a flag foi fornecida diretamente no enunciado
+flag = "crypto{y0ur_f1rst_fl4g}"
+print(f"Flag: {flag}")
